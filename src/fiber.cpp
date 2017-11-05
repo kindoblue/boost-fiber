b9c695eb241238b3e943ad1bebbb7b2d7b776243
Stefano S <kindoblue@gmail.com>
2017-11-05 22:02:07 +0100

    first commit

---
+++ src/fiber.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stefano on 05/11/2017.
+//
+
+#include "fiber.h"
---
+++ src/fiber.h
@@ -0,0 +1,14 @@
+//
+// Created by Stefano on 05/11/2017.
+//
+
+#ifndef FIBERTEST_FIBER_H
+#define FIBERTEST_FIBER_H
+
+
+class fiber {
+
+};
+
+
+#endif //FIBERTEST_FIBER_H
